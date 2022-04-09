e4ae1c7b1153cb1e27986d64f7a0cb9cefafa940
rboswellj <rboswellj@gmail.com>
2022-04-09 14:46:29 -0400

    DB is connected. Records pulling in to textboxes. Can cycle through them. Buttons for Adding and Modifying are present, just need to add the functionality.

---
+++ Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace RobertJohnsonEmployeesDatabase
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtJobTitle = new System.Windows.Forms.TextBox();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnLast = new System.Windows.Forms.Button();
+            this.btnFirst = new System.Windows.Forms.Button();
+            this.btnAddNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(180, 55);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(200, 39);
+            this.txtFirstName.TabIndex = 0;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(402, 55);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(200, 39);
+            this.txtLastName.TabIndex = 0;
+            // 
+            // txtJobTitle
+            // 
+            this.txtJobTitle.Location = new System.Drawing.Point(180, 145);
+            this.txtJobTitle.Name = "txtJobTitle";
+            this.txtJobTitle.Size = new System.Drawing.Size(200, 39);
+            this.txtJobTitle.TabIndex = 0;
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Location = new System.Drawing.Point(402, 145);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(200, 39);
+            this.txtDepartment.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(180, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Job Title";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(402, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 32);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Department";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(402, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(126, 32);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Last Name";
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(402, 209);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(150, 46);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.BtnNext_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(230, 209);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(150, 46);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.BtnBack_Click);
+            // 
+            // btnLast
+            // 
+            this.btnLast.Location = new System.Drawing.Point(558, 209);
+            this.btnLast.Name = "btnLast";
+            this.btnLast.Size = new System.Drawing.Size(150, 46);
+            this.btnLast.TabIndex = 2;
+            this.btnLast.Text = "Last";
+            this.btnLast.UseVisualStyleBackColor = true;
+            this.btnLast.Click += new System.EventHandler(this.BtnLast_Click);
+            // 
+            // btnFirst
+            // 
+            this.btnFirst.Location = new System.Drawing.Point(74, 209);
+            this.btnFirst.Name = "btnFirst";
+            this.btnFirst.Size = new System.Drawing.Size(150, 46);
+            this.btnFirst.TabIndex = 2;
+            this.btnFirst.Text = "First";
+            this.btnFirst.UseVisualStyleBackColor = true;
+            this.btnFirst.Click += new System.EventHandler(this.BtnFirst_Click);
+            // 
+            // btnAddNew
+            // 
+            this.btnAddNew.Location = new System.Drawing.Point(159, 293);
+            this.btnAddNew.Name = "btnAddNew";
+            this.btnAddNew.Size = new System.Drawing.Size(150, 46);
+            this.btnAddNew.TabIndex = 3;
+            this.btnAddNew.Text = "Add New";
+            this.btnAddNew.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(315, 293);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 46);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(471, 293);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 46);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAddNew);
+            this.Controls.Add(this.btnFirst);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnLast);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.txtJobTitle);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtJobTitle;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnLast;
+        private System.Windows.Forms.Button btnFirst;
+        private System.Windows.Forms.Button btnAddNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RobertJohnsonEmployeesDatabase
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DatabaseConnection objConnect;
+        string conString;
+
+        DataSet ds;
+        DataRow dRow;
+
+        int MaxRows;
+        int inc = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                objConnect = new DatabaseConnection();
+                // This is set up in the project settings. It is the connection string for the DB file
+                conString = Properties.Settings.Default.EmployeesConnectionString;
+
+                //Set connection string for new DB connection object
+                objConnect.connection_string = conString;
+                // Loads the SQL statement saved to settings into object (Select *)
+                objConnect.Sql = Properties.Settings.Default.SQL;
+                // Handing dataset over from connection
+                ds = objConnect.GetConnection;
+
+                // Our dataset only has one table. This gets row count on that table
+                MaxRows = ds.Tables[0].Rows.Count;
+
+                // Call method to populate the form fields with data from DB
+                NavigateRecords();
+            }
+            catch(Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void NavigateRecords()
+        {
+            dRow = ds.Tables[0].Rows[inc];
+            txtFirstName.Text = dRow.ItemArray.GetValue(1).ToString();
+            txtLastName.Text = dRow.ItemArray.GetValue(2).ToString();
+            txtJobTitle.Text = dRow.ItemArray.GetValue(3).ToString();
+            txtDepartment.Text = dRow.ItemArray.GetValue(4).ToString();
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            if(inc != MaxRows -1)
+            {
+                inc++;
+                NavigateRecords();
+            }
+            else
+            {
+                MessageBox.Show("No More Rows");
+            }
+        }
+
+        private void BtnBack_Click(object sender, EventArgs e)
+        {
+            if (inc > 0)
+            {
+                inc--;
+                NavigateRecords();
+            }
+            else
+            {
+                MessageBox.Show("First Record");
+            }
+        }
+
+        private void BtnFirst_Click(object sender, EventArgs e)
+        {
+            if(inc != 0)
+            {
+                inc = 0;
+                NavigateRecords();
+            }
+            else
+            {
+                MessageBox.Show("First Record");
+            }
+        }
+
+        private void BtnLast_Click(object sender, EventArgs e)
+        {
+            if(inc != MaxRows - 1)
+            {
+                inc = MaxRows - 1;
+                NavigateRecords();
+            }
+            else
+            {
+                MessageBox.Show("Last Record");
+            }
+        }
+    }
+}
